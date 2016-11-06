17b5239655d6d6f570ee2fa5d3eafd5f52945128
boomboompower <boomboompowerjunk@gmail.com>
2016-11-06 12:38:57 +1100

    Particles - 12:38PM

--- src/main/java/org/devathon/contest2016/PlayerListener.java
+++ src/main/java/org/devathon/contest2016/PlayerListener.java
@@ -1,10 +1,14 @@
 package org.devathon.contest2016;
 
+import net.minecraft.server.v1_10_R1.EnumParticle;
+import net.minecraft.server.v1_10_R1.PacketPlayOutWorldParticles;
+
 import org.bukkit.Bukkit;
-import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,7 +16,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.util.BlockIterator;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,12 +44,8 @@ public class PlayerListener implements Listener {
     private void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         blocks.stream().filter(block -> player.getLocation().distance(block.getLocation()) <= 6D).forEach(block -> {
-            BlockIterator iterator = new BlockIterator(player.getLocation());
-            Location location;
-            while (iterator.hasNext()) {
-                location = iterator.next().getLocation();
-                block.getWorld().playEffect(location, Effect.COLOURED_DUST, 0);
-            }
+            Location loc = block.getLocation();
+            spawn(player.getLocation(), block.getLocation());
         });
     }
 
@@ -59,6 +59,21 @@ public class PlayerListener implements Listener {
             Bukkit.getPluginManager().callEvent(turretCreateEvent);
 
             blocks.add(block);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    private void onTurretCreate(TurretCreateEvent event) {
+        ArmorStand stand = event.getLocation().getWorld().spawn(event.getLocation(), ArmorStand.class);
+        stand.setInvulnerable(true);
+        stand.setHelmet(new ItemStack(Material.STAINED_GLASS));
+        stand.setInvulnerable(true);
+    }
+
+    private void spawn(Location player, Location block) {
+        PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.REDSTONE, true, (float) block.getX(), (float) block.getY(), (float) block.getZ(), (float) player.getX(), (float) player.getY(), (float) player.getZ(), (float) 0, 0, 0);
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            ((CraftPlayer) all).getHandle().playerConnection.sendPacket(packet);
         }
     }
 }
